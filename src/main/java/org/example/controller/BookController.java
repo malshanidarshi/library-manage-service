7f05ecc282d826dc24d1426477b328a13dbd6ca1
malshanidarshi <mnidarshi02@gmail.com>
2024-02-13 19:51:55 +0530

    add book method

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -3,15 +3,20 @@ package org.example.controller;
 import org.example.dto.Book;
 import org.example.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @RequestMapping("/book")
+@RestController
 public class BookController {
     @Autowired
     BookService service;
-    public void addBook(Book book){
 
+    @PostMapping
+    public void addBook(@RequestBody Book book){
+        service.addBook(book);
     }
 }
